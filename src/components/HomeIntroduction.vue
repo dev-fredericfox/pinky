8b21011e8b73b9f6cd608437f48c0f9292d75697
Frederic Fox <dev.frederic.fox@gmail.com>
2021-12-07 20:48:07 +0100

    Change wording and cta location

--- src/components/HomeIntroduction.vue
+++ src/components/HomeIntroduction.vue
@@ -5,18 +5,18 @@
       <div class="column is-6 has-text-justified">
         <h1 class="title is-4 has-text-centered mb-5">Welcome to Pinky!</h1>
         <p>
-          This is a small project to work on my skill in Vue 3 Composition API
-          as well as in Machine Learning, especially with Neural Networks and
-          brain.js.
+          This is a small project to hone my Vue 3 Composition API skills as
+          well as to practice some Machine Learning, especially Neural Networks
+          with brain.js.
         </p>
         <p>
           Furthermore I used this project to learn about Bulma CSS, which I then
-          used in my Portfolio.
+          also used in my Portfolio.
         </p>
         <p>
-          Lastly, upon programming this app, I also unexpectedly learned about
-          parsing csvs, using JS Workers to offload some heavy work and making
-          files available for download
+          Lastly, upon programming this app, I also learned about parsing csvs,
+          using JS Workers to offload some heavy work and making files available
+          for download and writing to the browsers local storage.
         </p>
         <p>
           I have many more ideas to make this app better and add features, so
@@ -38,9 +38,13 @@
           Fred
         </p>
       </div>
-      <router-link class="navbar-item" to="/start">
-        <button class="button is-info">Try training a Neural Network</button>
-      </router-link>
+    </div>
+    <div class="columns is-centered">
+      <div class="column is-6 has-text-centered">
+        <router-link class="cta" to="/start">
+          <button class="button is-info">Try training a Neural Network</button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
