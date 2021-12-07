a33efc9bf969a01e97b9bccf31947b842f278730
Frederic Fox <dev.frederic.fox@gmail.com>
2021-12-07 16:27:32 +0100

    Add About content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="content p-5">
     <div class="about">
-      <h1>This is an about page, ok?</h1>
-      {{ count }}
-      <button class="button is-success" @click="count++">+1?</button>
+      <div class="columns is-centered mt-5">
+        <div class="column is-6">
+          <h1 class="title is-4">Personal Machine Learning Project</h1>
+          <p>
+            This is a small project to work on my Vue 3 Composition API skillas
+            and to improve my knowledege about Machine Learning, especially with
+            Neural Networks and brain.js.
+          </p>
+          <p>
+            Visit my full portfolio at:<br />
+            <a href="https://www.frederic-fox.com"
+              >https://www.frederic-fox.com</a
+            >
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
-
 export default {
   setup() {
-    const state = reactive({
-      count: 0,
-    });
-
-    return {
-      ...toRefs(state),
-    };
+    return {};
   },
 };
 </script>
