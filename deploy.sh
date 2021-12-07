4e7680cbed43836ca37aaa6dea87702d8b4ef412
Frederic Fox <dev.frederic.fox@gmail.com>
2021-12-07 20:28:28 +0100

    Add copy of index to 404 for GH SPA Issues

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,10 @@ cd dist
 # add .nojekyll to bypass GitHub Page's default behavior
 touch .nojekyll
 
+# Make a copy of index.html to handle GH-Pages weirdness
+cp index.html 404.html
+find . -name ".DS_Store" -delete
+
 # if you are deploying to a custom domain
 # echo 'www.frederic-fox.com' > CNAME
 
